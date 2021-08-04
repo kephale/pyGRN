e9e3ff6130dd0d06b4a5b004b86adcaf416a2b69
Kyle Harrington <mdc@kyleharrington.com>
2021-08-04 16:36:32 +0200

    Add conda environment file

--- setup.py
+++ setup.py
@@ -5,12 +5,7 @@ setup(
     name='pygrn',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'keras',
-        'numpy',
-        'PyYAML',
-        'tensorflow',
-        'tqdm'],
+    install_requires=["keras==2.2.4", 'numpy', 'PyYAML', 'tensorflow', 'tqdm'],
     license='',
     long_description=open('README.md').read(),
 )
